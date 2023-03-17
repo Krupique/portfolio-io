7f95758d34f879e408a85503bb1e8a78bc94b691
Henrique <krupique@outlook.com>
2023-03-16 21:47:13 -0300

    CHORE: Update experiences information

--- assets/js/script_translate_BR.js
+++ assets/js/script_translate_BR.js
@@ -115,12 +115,12 @@ $(document).ready(function(){
         $('#courses_4_a').text('Ir até o curso');
 
 
-        $('#courses_5_h4').text('Business Analytics with R and Python');
-        $('#courses_5_h5').text('July 2022');
+        $('#courses_5_h4').text('BUSINESS ANALYTICS COM R E PYTHON');
+        $('#courses_5_h5').text('Julho 2022');
         $('#courses_5_em').text('DSA - Data Science Academy');
-        $('#courses_5_p1').text('Data Science Academy Data Scientist Training Course 5/6');
-        $('#courses_5_p2').text('Business knowledge is one of the main skills of the Data Scientist. The objective of this course is to apply analytical techniques in business areas such as Marketing, Finance and HR, collecting data, defining metrics, creating models and extracting insights that generate value for companies and support decision-making.');
-        courses_5_ul = "<li>Predictive analytics</li><li>Marketing Analytics</li><li>HR Analytics</li><li>Financial Analytics</li><li>Social Network Analytics</li>";
+        $('#courses_5_p1').text('Curso 5/6 da Formação Cientista de Dados da Data Science Academy');
+        $('#courses_5_p2').text('Conhecimento de negócio é uma das habilidades principais do Cientista de Dados. O objetivo deste curso é aplicar técnicas analíticas em áreas de negócio como Marketing, Finanças e RH, coletando dados, definindo métricas, criando modelos e extraindo insights que gerem valor para as empresas e suportem as tomadas de decisões.');
+        courses_5_ul = "<li>Análise preditiva</li><li>Marketing Analytics</li><li>RH Analytics</li><li>Financial Analytics</li><li>Social Network Analytics</li>";
         $('#courses_5_ul').html(courses_5_ul);
         $('#courses_5_a').text('Ir até o curso');
 
