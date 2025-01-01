fafbbcf469161989e1ec35953e36b02d80cb871e
Krupique <krupique@outlook.com>
2025-01-01 18:44:35 -0300

    feat: translate the course to english

--- assets/js/script_translate_BR.js
+++ assets/js/script_translate_BR.js
@@ -287,7 +287,7 @@ $(document).ready(function(){
         $('#courses_21_a').text('Ir até o curso');
 
         $('#courses_22_h4').text('APRENDIZAGEM PROFUNDA PARA APLICAÇÕES DE INTELIGÊNCIA ARTIFICIAL COM PYTHON E C++');
-        $('#cursos_22_h5').text('Outubro de 2024');
+        $('#courses_22_h5').text('Outubro de 2024');
         $('#courses_22_em').text('DSA - Data Science Academy');
         $('#courses_22_p1').text('Curso 1/4 da Formação Engenheiro de IA da Data Science Academy');
         $('#courses_22_p2').html('Este curso é um programa abrangente e avançado em Inteligência Artificial (IA) e Aprendizado Profundo, projetado para fornecer conhecimento de ponta e habilidades práticas. Ele abrange conceitos básicos de IA, redes neurais e aplicações avançadas em áreas como Visão Computacional, Processamento de Linguagem Natural e Análise Financeira.<br/><br/>Com 10 projetos práticos, ele enfatiza o aprendizado prático, com foco em ferramentas de última geração, como os Transformers arquitetura e a plataforma Hugging Face. Sua abordagem de programação dupla integra a versatilidade do Python com o alto desempenho do C++, garantindo adaptabilidade para aplicativos em tempo real e implantação avançada de modelos.<br/><br/>O curso também apresenta um estudo de caso detalhado sobre o cofre uso do ChatGPT e inclui a criação de um Large Language Model (LLM) do zero, fornecendo uma base sólida para lidar com os desafios modernos da IA. Ele se destaca como um recurso de aprendizagem versátil e inovador.');
@@ -296,7 +296,7 @@ $(document).ready(function(){
         $('#courses_22_a').text('Ir para o curso');
 
         $('#courses_23_h4').text('IA GENERATIVA E LLMS PARA PROCESSAMENTO DE LINGUAGEM NATURAL');
-        $('#cursos_23_h5').text('Novembro de 2024');
+        $('#courses_23_h5').text('Novembro de 2024');
         $('#courses_23_em').text('DSA - Data Science Academy');
         $('#courses_23_p1').text('Curso 3/4 da Formação Engenheiro de IA da Data Science Academy');
         $('#courses_23_p2').html('O curso oferece uma jornada abrangente no campo da Inteligência Artificial (IA), com foco em IA Generativa, Modelos de Linguagem de Grande Escala (LLMs) e Processamento de Linguagem Natural (PLN). Ele aborda tópicos fundamentais e avançados, como Transformers, Few-Shot Learning, Transfer Learning, Fine-Tuning, PEFT, LoRa, RLHF, RAG e Llama 2. <br/><br/>Por meio de uma abordagem prática, os alunos explorarão tecnologias de ponta como OpenAI GPT, LangChain, LLMs Open-Source e AWS, aplicando esses conhecimentos em projetos reais para resolver desafios do mundo real.  <br/><br/>O curso é modular e combina teoria e prática, com capítulos bem estruturados seguidos de projetos práticos que garantem uma experiência imersiva. Ele busca capacitar os alunos para aproveitar o potencial da IA em um mercado de trabalho em constante evolução, destacando a importância de extrair insights e automatizar processos como uma vantagem competitiva essencial.');
@@ -305,12 +305,18 @@ $(document).ready(function(){
         $('#courses_23_a').text('Ir para o curso');
 
         $('#courses_24_h4').text('INTELIGÊNCIA ARTIFICIAL PARA VISÃO COMPUTACIONAL');
-        $('#cursos_24_h5').text('Dezembro de 2024');
+        $('#courses_24_h5').text('Dezembro de 2024');
         $('#courses_24_em').text('DSA - Data Science Academy');
         $('#courses_24_p1').text('Curso 2/4 da Formação Engenheiro de IA da Data Science Academy');
         $('#courses_24_p2').html('O curso explora a Visão Computacional, iniciando pelos fundamentos até técnicas avançadas, com foco prático no uso de ferramentas como a biblioteca HuggingFace, personalização de modelos pré-treinados, redes neurais convolucionais (CNNs) e transformers, incluindo Vision Transformers (ViT) e suas variações. Também aborda o estado da arte em IA, como a técnica "De Texto Para Imagem com Stable Diffusion".');
         $('#courses_24_a').text('Ir para o curso');
 
+        $('#courses_25_h4').text('ENGENHARIA FINANCEIRA COM INTELIGÊNCIA ARTIFICIAL');
+        $('#courses_25_h5').text('Janeiro de 2025');
+        $('#courses_25_em').text('DSA - Data Science Academy');
+        $('#courses_25_p1').text('Curso 4/4 da Formação Engenheiro de IA da Data Science Academy');
+        $('#courses_25_p2').html('O curso explora a interseção entre Finanças e Inteligência Artificial, abordando fundamentos da Engenharia Financeira e técnicas avançadas de IA aplicadas ao setor. Com uma estrutura modular, cobre desde temas básicos até tópicos avançados como hedging, derivativos e uso de LLMs. Os alunos desenvolvem habilidades práticas por meio de 10 projetos baseados em cenários reais, incluindo previsão de preços de ativos, análise de sentimento em notícias financeiras, detecção de fraudes e criação de robôs investidores. O foco é capacitar profissionais para lidar com a crescente demanda por análise de dados e IA no mercado financeiro.');
+        $('#courses_25_a').text('Ir para o curso');
         
 
         /*************** CONTACT ***************/
